5b8d1843b90ba2e736110915df3c0fe88d99f5f9
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-01-29 11:52:29 +0530

    forge-format

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -37,16 +37,12 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
         super.burn(_amount);
     }
 
-    function mint(
-        address _to,
-        uint256 _amount
-    ) external onlyOwner returns (bool) {
-
-        if(_to == address(0)) {
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
             revert DecentralizedStableCoin_NotZeroAddress();
         }
 
-        if(_amount <= 0) {
+        if (_amount <= 0) {
             revert DecentralizedStableCoin_MustBeMoreThanZero();
         }
 
